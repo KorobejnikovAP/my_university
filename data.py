352c57d8246f09b963c23074f2bd4b93c69b3f9e
KorobejnikovAP <armok2001@mail.ru>
2021-10-15 12:58:41 +0300

    version_2

--- data.py
+++ data.py
@@ -2,34 +2,45 @@ import pandas as pd
 import numpy as np
 from pathlib import *
 
-university = pd.read_csv(Path.cwd() / 'assets' / 'university.csv')
+salary = pd.read_csv(Path.cwd() / 'assets' / 'salary.csv')
 plan_learn = pd.read_csv(Path.cwd() / 'assets' / 'plan_learning.csv')
-ofline_un = pd.read_csv(Path.cwd() / 'assets' / 'ofline_un.csv')
-online_un = pd.read_csv(Path.cwd() / 'assets' / 'online_un.csv')
+rent = pd.read_csv(Path.cwd() / 'assets' / 'rent.csv')
+other_costs = pd.read_csv(Path.cwd() / 'assets' / 'other_costs.csv')
 
 # функция вычисляет затраты, необходимые как для онлайн, так и офлайн обучения 
 def university_costs():
     sum = 0
     #зарплата и ежеквартальные бонусы преподавателям 
     for i in range(8):
-        sum += plan_learn['coach_count'][i] * (university['salary_coach'][0] * 6 + 2 * university['bonus'][0])
+        sum += plan_learn['coach_count'][i] * (int(salary['salary'][salary.worker == 'coach']) * 6 + 2 * int(salary['bonus'][salary.worker == 'coach']))
     sum += sum * 0.3 
     #зарплата бухгалтера, методистов, системного администратора + бонусы
-    sum += 12 * 4 * (university['salary_accountant'][0] + 2 * university['salary_met'][0] + university['salary_admin'][0])
-    sum += 4 * 4 * 3 * university['bonus'][0]
+    sum += 12 * 4 * (int(salary['salary'][salary.worker == 'accountant']) + 2 * int(salary['salary'][salary.worker == 'methodist']) + int(salary['salary'][salary.worker == 'admin']))
+    bonus = 0
+    bonus += int(salary['bonus'][salary.worker == 'accountant'])
+    bonus += int(salary['bonus'][salary.worker == 'methodist'])
+    bonus += int(salary['bonus'][salary.worker == 'admin'])
+    sum += 4 * 4 * 3 * bonus
     #оборудование
-    sum += university['equipment'][0]
+    sum += other_costs['equipment'][0]
     return sum
 #функция вычисляет доп. затраты на офлайн обучение     
-def ofline_costs():
+def offline_costs():
     sum = 0
     #аренда всех момещений
-    sum += 4 * 12 * ofline_un['rent'][0] * ofline_un['square'][0]
+    sum += 4 * 12 * rent['rent_price'][0] * rent['square'][0]
     #зарплата охраннику и гардеробщице + ежеквартальные бонусы
-    sum += 4 * 12 * (ofline_un['salary_secur'][0] + ofline_un['salary_checkroom'][0]) + 4 * 3 * 2 * ofline_un['bonus'][0]
+    sum += 4 * 12 * (int(salary['salary'][salary.worker == 'security']) + int(salary['salary'][salary.worker == 'checkroom'])) 
+    bonus = 0
+    bonus += int(salary['bonus'][salary.worker == 'checkroom'])
+    bonus += int(salary['bonus'][salary.worker == 'security'])
+    sum += 4 * 3 * 2 * bonus
     return sum
 #функция вычисляет доп. затраты на онлай обучение 
-def online_coast():
+def online_costs():
     sum = 0
-    sum += 4 * 12 * (online_un['internet_price'][0] + online_un['zoom'][0])
-    return sum
+    sum += 4 * 12 * (other_costs['internet_price'][0] + other_costs['zoom'][0])
+    return sum 
+
+r = offline_costs()
+print(r)
--- web_app.py
+++ web_app.py
@@ -1,11 +1,42 @@
-import data as dt
+from dash.dependencies import Input, Output
 import dash 
 import dash_core_components as dcc
 import dash_html_components as html
-import numpy as np
+import plotly.express as px
+import data
+import pandas as pd
+from pathlib import *
+
+sum_offline_un = data.university_costs() + data.offline_costs()
+sum_online_un = data.university_costs() + data.online_costs()
+
+result = pd.DataFrame({
+    'student_count' : [i for i in range(100)],
+    'cost_of_study' : [0.0]*100,
+})
+for i in range(100):
+    result['cost_of_study'][i] = sum_offline_un / i
+result.to_csv(Path.cwd() / 'assets' / 'result.csv')
+
+fig = px.bar(data.salary, x = 'worker', y = ['salary', 'bonus'], labels = 'salary')
+fig2 = px.bar(data.plan_learn, x = 'semestr', y = 'coach_count', color = 'semestr')
+fig3 = px.line(result, x = 'student_count', y = 'cost_of_study')
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
-    html.H1('')
+    html.H1('Затраты на университет и стоимость образования'),
+    html.Div([
+        html.H2('Зарплаты сотрудников'),
+        dcc.Graph(figure = fig),
+    ]),
+    html.Div([
+        html.H2('Необходимое количество преподавателей на каждый семестр'),
+        dcc.Graph(figure = fig2),
+    ]),
+    html.Div([
+        html.H2('Стоимость обучения за 4 года'),
+        dcc.Graph(figure = fig3),
+    ]),
 ])
+
 app.run_server(debug=False)
